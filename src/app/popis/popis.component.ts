5cc710fc197a0cda8ba0d1161cd93bf12309e715
Stojance Nikolov <stojance_nikolov@digit.net.mk>
2023-01-13 15:48:38 +0100

    sn

--- src/app/popis/popis.component.ts
+++ src/app/popis/popis.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PopisService } from './popis.service';
 import { Observable, Subscription } from 'rxjs';
-import { OS_Sredstva } from '@app/models/HttpResponses/popis.responses';
+import { OS_Sredstva, V_Popisani } from '@app/models/HttpResponses/popis.responses';
 import { SharedModule } from '@app/@shared';
 
 @Component({
@@ -16,13 +16,20 @@ import { SharedModule } from '@app/@shared';
 export class PopisComponent implements OnInit, OnDestroy {
   isLoading$: Observable<boolean>;
   sredstva$: Observable<OS_Sredstva[]>;
+  popisani$: Observable<V_Popisani[]>;
   sredstvaSubscription: Subscription;
+  popisaniSubscription: Subscription;
+
   dtOptions: DataTables.Settings = {};
+  dtOptionsPopisani: DataTables.Settings = {};
+
   showTable: boolean = false;
+  showTablePopisani: boolean = false;
 
   constructor(private popisService: PopisService) {
     this.isLoading$ = this.popisService.isLoading$;
     this.sredstva$ = this.popisService.sredstva$;
+    this.popisani$ = this.popisService.popisani$;
   }
 
   ngOnInit(): void {
@@ -44,16 +51,50 @@ export class PopisComponent implements OnInit, OnDestroy {
       ],
       data: [],
     };
+    this.dtOptionsPopisani = {
+      pagingType: 'full_numbers',
+      columns: [
+        {
+          title: 'Ured broj',
+          data: 'UredBroj',
+        },
+        {
+          title: 'ID',
+          data: 'OS_ID',
+        },
+        {
+          title: 'Inventaren broj',
+          data: 'InventarenBroj',
+        },
+        {
+          title: 'Naziv',
+          data: 'Naziv',
+        },
+      ],
+      data: [],
+    };
     this.sredstvaSubscription = this.sredstva$.subscribe((data) => {
       this.dtOptions = Object.assign(this.dtOptions, { data });
+      this.showTable = data.length > 0;
+      this.showTablePopisani = false;
+    });
+    this.popisaniSubscription = this.popisani$.subscribe((data) => {
+      this.dtOptionsPopisani = Object.assign(this.dtOptionsPopisani, { data });
+      this.showTablePopisani = data.length > 0;
+      this.showTable = false;
     });
   }
 
   ngOnDestroy(): void {
     this.sredstvaSubscription.unsubscribe();
+    this.popisaniSubscription.unsubscribe();
   }
 
   loadSredstva() {
     this.popisService.loadSredstva();
+  }
+
+  loadPopisani() {
+    this.popisService.loadPopisani();
   }
 }
--- src/app/popis/popis.service.ts
+++ src/app/popis/popis.service.ts
@@ -2,28 +2,44 @@ import { Injectable } from '@angular/core';
 import { Observable, of, Subject, BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { AppConfigService } from '@shared/services/app-config.service';
-import { OS_Sredstva } from '@app/models/HttpResponses/popis.responses';
+import { OS_Sredstva, V_Popisani } from '@app/models/HttpResponses/popis.responses';
 
 @Injectable()
 export class PopisService {
   private _sredstvaSubject: BehaviorSubject<Array<OS_Sredstva>>;
+  private _popisaniSubject: BehaviorSubject<Array<V_Popisani>>;
   private _isLoadingSubject: BehaviorSubject<boolean>;
 
   constructor(private httpClient: HttpClient, private appConfigService: AppConfigService) {
     this._sredstvaSubject = new BehaviorSubject<Array<OS_Sredstva>>([]);
+    this._popisaniSubject = new BehaviorSubject<Array<V_Popisani>>([]);
     this._isLoadingSubject = new BehaviorSubject(false);
   }
 
   loadSredstva() {
     this._isLoadingSubject.next(true);
     this.httpClient.get<Array<OS_Sredstva>>(`${this.appConfigService.popisTablesUrl}/os_sredstva`).subscribe((res) => {
+      this._popisaniSubject.next([]);
       this._sredstvaSubject.next(res);
+      this._isLoadingSubject.next(false);
+    });
+  }
+
+  loadPopisani() {
+    this._isLoadingSubject.next(true);
+    this.httpClient.get<Array<V_Popisani>>(`${this.appConfigService.popisTablesUrl}/v_popisani`).subscribe((res) => {
+      this._sredstvaSubject.next([]);
+      this._popisaniSubject.next(res);
       this._isLoadingSubject.next(false);
     });
   }
 
   get sredstva$() {
     return this._sredstvaSubject.asObservable();
+  }
+
+  get popisani$() {
+    return this._popisaniSubject.asObservable();
   }
 
   get isLoading$() {
